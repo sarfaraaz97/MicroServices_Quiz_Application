5d7c5024b4c6e6c6d01e9f72b2a13fbea6a46356
sarfaraaz97 <214G1A3297@srit.ac.in>
2025-08-12 13:09:06 +0530

    good

--- QuizService/src/main/java/org/example/quizservice/Repo/QuizRepo.java
+++
@@ -1,9 +0,0 @@
-package org.example.quizservice.Repo;
-
-
-import org.example.quizservice.Model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepo extends JpaRepository<Quiz,Integer> {
-
-}
